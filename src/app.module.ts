78a2394e2d86710d9e7f4665d66e194eb1260da2
hjeongb0320 <hjeongb0320@naver.com>
2024-01-05 14:10:24 +0900

    feat: firebase module 위치 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { FirebaseModule } from 'firebase/firebase.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, FirebaseModule],

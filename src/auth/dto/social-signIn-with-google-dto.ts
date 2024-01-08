07fef32288cec529e2f3662daf74992c8c186024
saewoohan <112225610+saewoohan@users.noreply.github.com>
2024-01-08 22:27:04 +0900

    Feature/login saewoo (#13)
    * fix: fixed URL
    
    * fix: fixed google login
    
    ---------
    
    Co-authored-by: Jeong Beom Hong <80691668+hjeongb0320@users.noreply.github.com>

---
+++ src/auth/dto/social-signIn-with-google-dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class SocialSignInWithGoogleDTO {
+  @ApiProperty({
+    example: 'hsoooo2341@gmail.com',
+    description: '이메일',
+  })
+  email: string;
+
+  @ApiProperty({
+    example: '한승우',
+    description: '사용자의 이름',
+  })
+  name: string;
+
+  @ApiProperty({
+    example:
+      'https://lh3.googleusercontent.com/a/ACg8ocJJG2Yc3y1ogPldjkcKNHZPzzBxazUYyxC6MSrdojFz=s1337',
+    description: '프로필 이미지',
+  })
+  profileImage: string;
+
+  @ApiProperty({
+    example: '여기에_FCM_토큰을_입력',
+    description: 'FCM 토큰',
+  })
+  fcm_token: string;
+}
--- src/auth/dto/social-signIn-with-kakao-dto.ts
+++ src/auth/dto/social-signIn-with-kakao-dto.ts
@@ -1,18 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { AuthProvider } from '../types/auth-provider.type';
 
-export class SocialSignInDTO {
+export class SocialSignInWithKakaoDTO {
   @ApiProperty({
     example: '0ZK6WTjgrMYdaUSSO-Utmcvr4DXHp5HIdC4KPXTbAAABjNrxylZb9Pmr5eg_ZA',
     description: '소셜 서비스에서 제공하는 액세스 토큰',
   })
   access_token: string;
-
-  @ApiProperty({
-    example: 'kakao',
-    description: '소셜 서비스 제공자 (예: "kakao", "google", "facebook")',
-  })
-  provider: AuthProvider;
 
   @ApiProperty({
     example: '여기에_FCM_토큰을_입력',

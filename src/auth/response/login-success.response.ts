fe0fdef8e657e5fb4c139ec64533ea3357097de3
saewoohan <hso2341@hanyang.ac.kr>
2024-01-08 22:45:17 +0900

    Squashed commit of the following:
    commit 4d8810ebeb72a6c4b5641a17d155a51eb5c34346
    Author: saewoohan <112225610+saewoohan@users.noreply.github.com>
    Date:   Mon Jan 8 22:27:04 2024 +0900
    
        Feature/login saewoo (#13)
    
        * fix: fixed URL
    
        * fix: fixed google login
    
        ---------
    
        Co-authored-by: Jeong Beom Hong <80691668+hjeongb0320@users.noreply.github.com>

--- src/auth/response/login-success.response.ts
+++ src/auth/response/login-success.response.ts
@@ -1,17 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { UserToken } from '../types/user-token.type';
 
 export class LoginSuccessResponse {
   @ApiProperty({
-    example: {
-      code: 1,
-      message: 'Success',
-      data: {
-        uid: 'Oz7yE9BRsCRArkTRrAiPJFKa1BA3',
-        token:
-          'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwNDQ4MTIzOCwiZXhwIjoxNzA0NDg0ODM4LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay0zZDh4NEB0cmFiLTkyYWZmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstM2Q4eDRAdHJhYi05MmFmZi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6Ik96N3lFOUJSc0NSQXJrVFJyQWlQSkZLYTFCQTMifQ.X4_MHPFOVKkhR_04BPC6pX4hIGR8Np2vR71heq6zirrOgm4EFgzTG0Y_tP8n51kj_ZbaftW4h1W6VIwj-uY83hrbRPAaCWwUQ6FK-7tzDYMI4ypKX0bJ80MsAu3JPvG7sIyvSIlgzQXxULXmBsXvz-uaRLOkdCMaIdHHnLzyEf84LRlEPovRFqVNldskfCTI60axL0VCCL9_IjtCjZX29yXLkVpy3WSHgfAUgZmetbQufnczi3glJJappZecJNctPUkdRcZVMye4P6LdhmORm_tp6q4qFllMOpOI16c5NUrSCm5QGGG7gZxQq8FOw4J1uh8xkNCZzhHfiqOXgQcW3Q',
-      },
-    },
-    description: '로그인 성공',
+    example: 1,
   })
-  uid: string;
+  code: number;
+  @ApiProperty({
+    example: 'Success',
+  })
+  message: string;
+  @ApiProperty({
+    example: {
+      uid: 'Oz7yE9BRsCRArkTRrAiPJFKa1BA3',
+      token:
+        'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGVhcGlzLmNvbS9nb29nbGUuaWRlbnRpdHkuaWRlbnRpdHl0b29sa2l0LnYxLklkZW50aXR5VG9vbGtpdCIsImlhdCI6MTcwNDQ4MTIzOCwiZXhwIjoxNzA0NDg0ODM4LCJpc3MiOiJmaXJlYmFzZS1hZG1pbnNkay0zZDh4NEB0cmFiLTkyYWZmLmlhbS5nc2VydmljZWFjY291bnQuY29tIiwic3ViIjoiZmlyZWJhc2UtYWRtaW5zZGstM2Q4eDRAdHJhYi05MmFmZi5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInVpZCI6Ik96N3lFOUJSc0NSQXJrVFJyQWlQSkZLYTFCQTMifQ.X4_MHPFOVKkhR_04BPC6pX4hIGR8Np2vR71heq6zirrOgm4EFgzTG0Y_tP8n51kj_ZbaftW4h1W6VIwj-uY83hrbRPAaCWwUQ6FK-7tzDYMI4ypKX0bJ80MsAu3JPvG7sIyvSIlgzQXxULXmBsXvz-uaRLOkdCMaIdHHnLzyEf84LRlEPovRFqVNldskfCTI60axL0VCCL9_IjtCjZX29yXLkVpy3WSHgfAUgZmetbQufnczi3glJJappZecJNctPUkdRcZVMye4P6LdhmORm_tp6q4qFllMOpOI16c5NUrSCm5QGGG7gZxQq8FOw4J1uh8xkNCZzhHfiqOXgQcW3Q',
+    },
+  })
+  data: UserToken;
 }

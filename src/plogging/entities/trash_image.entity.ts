916dc346591585e10e10d9c4cabfda10716a5bcc
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-16 19:18:41 +0900

    feat: user crud

--- src/plogging/entities/trash_image.entity.ts
+++ src/plogging/entities/trash_image.entity.ts
@@ -1,14 +1,25 @@
-import {Entity, Column, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
-import { Plogging } from "./plogging.entity";
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  JoinColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Plogging } from './plogging.entity';
 
 @Entity('trash_image')
 export class Trash_image {
-    @PrimaryGeneratedColumn()
-    key : number;
+  @PrimaryGeneratedColumn()
+  key: number;
 
-    @Column({type : 'varchar', nullable : false})
-    trash_tag : string;
+  @Column({ type: 'varchar', nullable: false })
+  trash_tag: string;
 
-    @ManyToOne(() => Plogging, (plogging) => plogging.id)
-    id : number;
+  @DeleteDateColumn({ type: 'timestamp', name: 'deleted_at', nullable: true })
+  deletedAt: Date | null;
+
+  @ManyToOne(() => Plogging, (plogging) => plogging.plogging_id)
+  @JoinColumn({ name: 'plogging_id' })
+  plogging: Plogging;
 }

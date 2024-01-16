64b14548feac5ecbe89e51bd642b44e442213bae
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-16 16:22:36 +0900

    feat: user_info_get 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,33 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
+import { UserInfoDto } from './dto/user-info.dto';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('/info')
+  @ApiOperation({
+    summary: '유저 정보를 가져오는 api',
+  })
+  @ApiResponse({
+    status: 200,
+    description: '성공: 유저 정보 반환',
+    type: UserInfoDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: '실패: 유저를 찾을 수 없음',
+  })
+  @ApiResponse({
+    status: 500,
+    description: '실패: 서버 자체 오류',
+  })
+  async getUserInfo(
+    @Headers('Authorization') idToken: string,
+  ): Promise<UserInfoDto> {
+    return this.userService.getUserInfo(idToken);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { UserInfoDto } from './dto/user-info.dto';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class UserService {
@@ -10,16 +16,55 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
-  async createUser(email: string): Promise<void> {
+  async createUser(
+    uid: string,
+    email: string,
+    image: string | null,
+  ): Promise<void> {
     try {
       if (!email) {
         throw new NotAcceptableException('이메일 입력이 없습니다.');
       }
 
-      const user: User = new User();
-      user.email = email;
+      const user: User = {
+        uid,
+        email,
+        image,
+      };
 
       await this.userRepository.save(user);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getUserInfo(idToken: string): Promise<UserInfoDto> {
+    try {
+      const bearerIdToken: string = idToken.substring(7);
+      const decodedIdToken: any = jwt.decode(bearerIdToken);
+
+      const uid: string = decodedIdToken.uid;
+
+      const user = await this.userRepository.find({
+        select: {
+          email: true,
+          image: true,
+        },
+        where: {
+          uid: uid,
+        },
+      });
+
+      if (user.length === 0) {
+        throw new NotFoundException('유저를 찾을 수 없습니다.');
+      }
+
+      const ret: UserInfoDto = {
+        user_email: user[0].email,
+        user_image: user[0].image,
+      };
+
+      return ret;
     } catch (error) {
       throw error;
     }

b9e81faba1c1e1f92378b878a12ad7d218704008
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-21 15:09:18 +0900

    feat: plogging-insert-api

--- src/plogging/plogging.module.ts
+++ src/plogging/plogging.module.ts
@@ -3,9 +3,11 @@ import { PloggingController } from './plogging.controller';
 import { PloggingService } from './plogging.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Plogging } from './entities/plogging.entity';
+import { UserModule } from 'src/user/user.module';
+import { ImageModule } from 'src/image/image.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Plogging])],
+  imports: [TypeOrmModule.forFeature([Plogging]), UserModule, ImageModule],
   controllers: [PloggingController],
   providers: [PloggingService],
   exports: [PloggingService],
--- src/plogging/plogging.service.ts
+++ src/plogging/plogging.service.ts
@@ -2,12 +2,19 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Plogging } from './entities/plogging.entity';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PloggingInfoDto } from './dto/plogging-info.dto';
+import { decodeToken } from 'src/common/utils/decode-idtoken';
+import { UserService } from 'src/user/user.service';
+import { User } from 'src/user/entities/user.entity';
+import { ImageService } from 'src/image/image.service';
 
 @Injectable()
 export class PloggingService {
   constructor(
     @InjectRepository(Plogging)
     private readonly ploggingRepository: Repository<Plogging>,
+    private readonly userService: UserService,
+    private readonly imageService: ImageService,
   ) {}
 
   async getPloggingByUserIdAndPloggingId(uid: string, ploggingId: number): Promise<Plogging> {
@@ -32,5 +39,42 @@ export class PloggingService {
     } catch (error) {
       throw error;
     }
+  }
+
+  async uploadPlogging(
+    idToken: string,
+    images: Array<Express.Multer.File>,
+    ploggingInfo: PloggingInfoDto,
+  ): Promise<User> {
+    try {
+      const uid: string = await decodeToken(idToken);
+
+      const trabSnack: number = images.length;
+      const user: User = await this.userService.getUserByUserId(uid);
+
+      const newPlogging = await this.ploggingRepository
+        .createQueryBuilder()
+        .insert()
+        .into(Plogging)
+        .values([
+          {
+            user: user,
+            run_date: ploggingInfo.runDate,
+            run_name: ploggingInfo.runName,
+            run_range: ploggingInfo.runRange,
+            run_time: ploggingInfo.runTime,
+            trab_snack: trabSnack,
+            calorie: 0, // 아직 칼로리 처리는 안함
+          },
+        ])
+        .execute();
+
+      await this.imageService.uploadPloggingTrashImages(uid, newPlogging.generatedMaps[0].plogging_id, images);
+
+      return newPlogging[0];
+    } catch (error) {
+      throw error;
+    }
+    return;
   }
 }

6cbd078e56dce234beb484ff444221909d69c5f9
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-22 01:07:14 +0900

    feat: trab-api 구현

--- src/trab/trab.module.ts
+++ src/trab/trab.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TrabController } from './trab.controller';
+import { TrabService } from './trab.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Trab } from './entities/trab.entity';
+import { Furniture } from './entities/furniture.entity';
+import { UserModule } from 'src/user/user.module';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Trab, Furniture]), UserModule],
+  controllers: [TrabController],
+  providers: [TrabService],
+  exports: [TrabService],
+})
 export class TrabModule {}

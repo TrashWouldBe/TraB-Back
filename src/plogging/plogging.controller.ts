7a409d5f4d457a84beb7a944881c697c2e2c89d9
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-22 04:15:55 +0900

    feat: plogging 이미지 올리는 api test 완료

--- src/plogging/plogging.controller.ts
+++ src/plogging/plogging.controller.ts
@@ -1,48 +1,26 @@
-import { Body, Controller, Post, Req, UploadedFile, UseInterceptors } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, Query, Req, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PloggingService } from './plogging.service';
 import { PloggingInfoDto } from './dto/plogging-info.dto';
 import { User } from 'src/user/entities/user.entity';
 import { serializeMessage } from 'src/common/utils/serialize-message';
 import { SUCCESS_CODE } from 'src/common/constants/constants';
 import { SerializedMessage } from 'src/common/types/serialized-message.type';
+import { FilesUploadDto } from './dto/plogging-upload.dto';
 
 @ApiTags('Plogging')
 @Controller('plogging')
 export class PloggingController {
   constructor(private readonly ploggingService: PloggingService) {}
 
-  @UseInterceptors(FileInterceptor('images'))
+  @UseInterceptors(FilesInterceptor('images'))
   @Post()
   @ApiBearerAuth('id_token')
   @ApiOperation({
     summary: '[미완]플로깅 정보를 저장하는 api',
   })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({
-    schema: {
-      type: 'object',
-      properties: {
-        images: {
-          type: 'array',
-          items: {
-            type: 'string',
-            format: 'binary',
-          },
-        },
-        ploggingInfo: {
-          type: 'object',
-          properties: {
-            runDate: { type: 'string' },
-            runNane: { type: 'string' },
-            runRange: { type: 'number' },
-            runTime: { type: 'string' },
-          },
-        },
-      },
-    },
-  })
   @ApiResponse({
     status: 201,
     description: '성공: 이미지 업로드 성공',
@@ -57,11 +35,11 @@ export class PloggingController {
   })
   async uploadPloggingTrashImage(
     @Req() request: Request,
-    @UploadedFile() images: Array<Express.Multer.File>,
-    @Body('ploggingInfo') ploggingInfo: PloggingInfoDto,
+    @UploadedFiles() images: Array<Express.Multer.File>,
+    @Body() filesUploadDto: FilesUploadDto,
   ): Promise<SerializedMessage> {
     const token: string = request.headers['authorization'];
-    const data: User = await this.ploggingService.uploadPlogging(token, images, ploggingInfo);
+    const data: User = await this.ploggingService.uploadPlogging(token, images, filesUploadDto);
     return serializeMessage({
       code: SUCCESS_CODE,
       message: 'Success',

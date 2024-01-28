1ef2f8d47937653933d47d0affde86afd7f78098
Jeong Beom Hong <hjeongb0320@naver.com>
2024-01-28 11:19:31 +0900

    fix: user api

---
+++ src/user/dto/get-user-name-and-weight.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class GetUserNameAndWeightDto {
+  @ApiProperty({
+    example: '유저 이름',
+  })
+  name: string;
+
+  @ApiProperty({
+    example: '유저 몸무게 (정수)',
+  })
+  weight: number;
+}
